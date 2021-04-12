874c0ec8542a0cdc1230386e37627e91096e535e
Ivan <raggamix@mail.ru>
2021-04-12 21:38:43 +0300

    feat: add dynamic context switch

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -23,10 +23,21 @@ import './app.css';
 
 export default class App extends Component {
 
-  swapiService = new DummySwapiService();
-
   state = {
-    showRandomPlanet: true
+    showRandomPlanet: true,
+    swapiService: new DummySwapiService()
+  };
+
+  onServiceChange = () => {
+    this.setState(({ swapiService }) => {
+
+      const Service = swapiService instanceof SwapiService ?
+                        DummySwapiService : SwapiService;
+
+      return {
+        swapiService: new Service()
+      };
+    });
   };
 
   toggleRandomPlanet = () => {
@@ -46,9 +57,7 @@ export default class App extends Component {
     const { getPerson,
             getStarship,
             getPersonImage,
-            getStarshipImage,
-            getAllPeople,
-            getAllPlanets } = this.swapiService;
+            getStarshipImage } = this.state.swapiService;
 
     const personDetails = (
       <ItemDetails
@@ -76,9 +85,9 @@ export default class App extends Component {
 
     return (
       <ErrorBoundry>
-        <SwapiServiceProvider value={this.swapiService} >
+        <SwapiServiceProvider value={this.state.swapiService} >
           <div className="stardb-app">
-            <Header />
+            <Header onServiceChange={this.onServiceChange} />
 
             <PersonDetails itemId={11} />
 
