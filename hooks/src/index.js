eb734693cefbc92fabd5326a7248c2d7b40f4239
Ivan <raggamix@mail.ru>
2021-04-20 20:12:37 +0300

    feat: add useState hook

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <p>It's Alive!</p>;
+const App = () => {
+  return (
+    <div>
+      <HookSwitcher />
+    </div>
+  );
+};
+
+const HookSwitcher = () => {
+
+  const [ color, setColor ] = useState('gray');
+  const [ fontSize, setFontSize ] = useState(14);
+
+  return (
+    <div style={{ padding: '10px',
+     backgroundColor: color,
+     fontSize: `${fontSize}px` }}>
+       It's Alive
+      <button
+        onClick={() => setColor('gray')}>
+        Dark
+      </button>
+      <button
+        onClick={() => setColor('white')}>
+        Light
+      </button>
+      <button
+        onClick={() => setFontSize((s) => s + 2)}>
+        +
+      </button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
