a847542e544ab4c4bbdc3e9efa16d758ac952dd4
Ivan <raggamix@mail.ru>
2021-04-24 22:39:18 +0300

    feat: useeffect for get data and clear result

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const [value, setValue] = useState(0);
+    const [value, setValue] = useState(1);
     const [visible, setVisible] = useState(true);
 
     if (visible) {
@@ -12,8 +12,7 @@ const App = () => {
                     onClick={() => setValue((v) => v + 1)}>+</button>
                 <button 
                     onClick={() => setVisible(false)}>hide</button>
-                {/* <HookCounter value={value} /> */}
-                <Notification />
+                <PlanetInfo id={value} />
             </div>
         );
     } else {
@@ -21,42 +20,21 @@ const App = () => {
     }
 };
 
-const Notification = () => {
-
-    const [visibleNotif, setVisibleNotif] = useState(true);
+const PlanetInfo = ( {id} ) => {
+    const [name, setName] = useState(null);
 
     useEffect(() => {
-        const timeout = setTimeout(
-            () => setVisibleNotif(false), 2500);
-        return () => clearTimeout(timeout);
-    }, [ ])
+        let cancelled = false;
+        fetch(`https://swapi.dev/api/planets/${id}/`)
+            .then(res => res.json())
+            .then(data => !cancelled && setName(data.name));
+        return () => cancelled = true;
+    }, [id]);
 
     return (
-        <div>
-            { visibleNotif && <p>Hello</p> }
-        </div>
+        <div>{id} - {name}</div>
     );
 };
-
-// const HookCounter = ({ value }) => {
-
-//     useEffect(() => {
-//         console.log('mount');
-//     }, []);
-    
-//     useEffect(() => {
-//         console.log('update');
-//     });
-
-//     useEffect(() => {
-
-//         return (() => {
-//             console.log('unmount');
-//         })
-//     }, []);
-
-//     return <p> {value} </p>;
-// };
 
 ReactDOM.render(
     <React.StrictMode>
